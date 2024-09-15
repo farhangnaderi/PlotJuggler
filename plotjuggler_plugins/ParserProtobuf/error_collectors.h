591cb1d42e1e0183b9f6fc2b8c80ef7bbf134a88
Matthias Grob <maetugr@gmail.com>
2024-09-15 18:33:39 +0200

    Building fine on MacOS 14.6

--- plotjuggler_plugins/ParserProtobuf/error_collectors.h
+++ plotjuggler_plugins/ParserProtobuf/error_collectors.h
@@ -10,10 +10,10 @@ class IoErrorCollector : public google::protobuf::io::ErrorCollector
 {
 public:
   void AddError(int line, google::protobuf::io::ColumnNumber column,
-                const std::string& message) override;
+                const std::string& message);
 
   void AddWarning(int line, google::protobuf::io::ColumnNumber column,
-                  const std::string& message) override;
+                  const std::string& message);
 
   const QStringList& errors()
   {
@@ -28,10 +28,10 @@ class FileErrorCollector : public google::protobuf::compiler::MultiFileErrorColl
 {
 public:
   void AddError(const std::string& filename, int line, int,
-                const std::string& message) override;
+                const std::string& message);
 
   void AddWarning(const std::string& filename, int line, int,
-                  const std::string& message) override;
+                  const std::string& message);
 
   const QStringList& errors()
   {
